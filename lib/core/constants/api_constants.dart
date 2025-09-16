3e4db51c525d71428b436e3bde65b7d764aef483
FAVOUR PC <fosas006@gmail.com>
2025-09-16 15:02:40 +0100

    APIKey, AndroidManifest.xml, Info.plist changes

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
-  static const String apiKey = 'YOUR_API_KEY_HERE'; // Replace with your OpenWeatherMap API key
+  static const String apiKey = '7436fffbcdf7f464bc6610e24892ae6d'; // Replace with your OpenWeatherMap API key
   static const String currentWeatherEndpoint = '/weather';
   static const String forecastEndpoint = '/forecast';
   static const String iconUrl = 'https://openweathermap.org/img/wn/';
